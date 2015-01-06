d4bc0b88a0745015f2e8e5d9253fad415abfdb14
v.backx@gmail.com <v.backx@gmail.com>
2015-01-06 22:10:34 +0100

    ajout queueparam

---
+++ QueueParam.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+
+public class QueueParam {
+	
+	public const string ADD = "ADD";
+	public const string REMOVE = "REMOVE";
+	
+	public string type;
+	public string eventType;
+	public Action<object[]> callback;
+    public string context;
+
+	public QueueParam(string t, string e,Action<object[]> c,string context)
+	{
+		this.type = t;
+		this.eventType = e;
+		this.callback = c;
+        this.context = context;
+	}
+}
